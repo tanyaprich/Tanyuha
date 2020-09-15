9682a14c54f267819be24b5ee571fc4dfc4a4a30
tanyaprich <70521755+tanyaprich@users.noreply.github.com>
2020-09-15 07:37:42 +0300

    Add files via upload
    This program's really unique. You can get any pinguins you want

---
+++ pinguins2.py
@@ -0,0 +1,11 @@
+a =   '    _~_     '
+b =   '   (o o)    '
+c =  '  /  V  \\   '
+d = ' /(  _  )\\  '
+e =   '   ^^ ^^    '
+f = int(input())
+print(a*f)
+print(b*f)
+print(c*f)
+print(d*f)
+print(e*f)
